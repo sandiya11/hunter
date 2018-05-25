64f5249fea47d9075a5c700fa3f665572dc2f54d
sandiya11 <36365541+sandiya11@users.noreply.github.com>
2018-05-25 15:24:48 +0530

    Create 26.cpp

---
+++ 26.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+int a[50],i,n,m;
+cin>>n;
+for(i=0;i<n;i++)
+{
+    cin>>a[i];
+}
+for(i=n-1;i>=0;i--)
+{
+    cout<<a[i]<<"->";
+}
+    return 0;
+}
